a8d66498bf9fa09c6e40e31b65143bd4b70a47bd
izzahnabilah <nqa5448@psu.edu>
2024-09-13 15:21:01 -0400

    Update write_ids.c

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "Izzahnabilah";
+    char STUDENT_ID[] = "997357114";
 
     return 0 ;
 }
